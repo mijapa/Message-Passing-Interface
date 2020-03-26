76f2fd5d840e621ec42f43e228374d0ce663cf8e
Michał <michaljanpatyk@gmail.com>
2020-03-26 16:07:41 +0100

    change program file names in batch file

--- lab4/mpi_pi_batch_processing_Sc_NonSc.sh
+++ lab4/mpi_pi_batch_processing_Sc_NonSc.sh
@@ -11,13 +11,13 @@
 #SBATCH --mail-user=michaljanpatyk@gmail.com
 
 module add plgrid/tools/openmpi
-mpicc -std=c11 -o reduce_pi reduce_pi.c
+mpicc -std=c11 -o mpi_pi_reduce mpi_pi_reduce.c
 
 echo "n, p, s, pi" > non_scalable.csv
 for ((i=1; i<=$1; i++)); do
   echo "$i"
   for ((j=1; j<=$3; j++)); do
-      mpiexec -np "$i" ./reduce_pi "$2" "non_scalable.csv"
+      mpiexec -np "$i" ./mpi_pi_reduce "$2" "non_scalable.csv"
   done
 done
 
@@ -26,6 +26,6 @@ for ((i=1; i<=$1; i++)); do
   echo "$i"
   for ((j=1; j<=$3; j++)); do
     ((k = $(expr $2 \* $i)))
-    mpiexec -np "$i" ./reduce_pi "$k" "scalable.csv"
+    mpiexec -np "$i" ./mpi_pi_reduce "$k" "scalable.csv"
   done
 done
