ca42c32846fb708f94747dfb861056d7458dca94
Michał <michaljanpatyk@gmail.com>
2020-03-26 17:41:01 +0100

    change execution time, correct bash file

--- lab4/mpi_pi_batch_processing_Sc_NonSc.sh
+++ lab4/mpi_pi_batch_processing_Sc_NonSc.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes 1
 #SBATCH --ntasks 12
 #SBATCH --exclusive
-#SBATCH --time=00:01:00
+#SBATCH --time=00:05:00
 #SBATCH --constraint="intel"
 #SBATCH --mem-per-cpu=100MB
 #SBATCH --partition=plgrid
@@ -13,7 +13,7 @@
 module add plgrid/tools/openmpi
 mpicc -std=c11 -o mpi_pi_reduce mpi_pi_reduce.c
 
-echo "n, p, s, pi" > non_scalable.csv
+echo "n,p,s,pi" > non_scalable.csv
 for ((i=1; i<=$1; i++)); do
   echo "$i"
   for ((j=1; j<=$3; j++)); do
@@ -21,7 +21,7 @@ for ((i=1; i<=$1; i++)); do
   done
 done
 
-echo "n, p, s, pi" > scalable.csv
+echo "n,p,s,pi" > scalable.csv
 for ((i=1; i<=$1; i++)); do
   echo "$i"
   for ((j=1; j<=$3; j++)); do
